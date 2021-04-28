58ca5e1ac9091ff42eed488a01fa2b8a9c45c210
JesNatTer <82204857+JesNatTer@users.noreply.github.com>
2021-04-28 16:22:17 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,4 @@
-repetitions = int(input("Enter number of fibonacci sequence repetitions: "))  # number of reps
+numbers = 20  # amount of numbers to be displayed
 
 
 def fibonacci(n):  # function for fibonacci sequence
@@ -8,9 +8,9 @@ def fibonacci(n):  # function for fibonacci sequence
         return fibonacci(n-1) + fibonacci(n-2)  # function formula for fibonacci
 
 
-if repetitions <= 0:
-    print("Please enter positive value")  # number of repetitions cannot be less than 1
+if numbers <= 0:
+    print("Please enter positive value")  # amount of numbers cannot be less than 1
 else:
-    for i in range(repetitions):  # number of reps is 1 or more, so sequence is printed
+    for i in range(numbers):  # amount of numbers is 1 or more, so sequence is printed
         print(fibonacci(i))
 
